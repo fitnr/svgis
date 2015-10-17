b3017dc20e98b7b41b882f7782a07ad6b3b5ed58
fitnr <fitnr@fakeisthenewreal>
2015-10-17 15:32:24 -0400

    bump 0.1.2

---
+++ HISTORY.rst
@@ -0,0 +1,4 @@
+0.1.2
+-----
+
+* Add --no-viewbox option to create translated SVGs, rather than viewboxed ones
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ except IOError:
 setup(
     name='svgis',
 
-    version='0.1.1',
+    version='0.1.2',
 
     description='Draw geodata in SVG',
 
