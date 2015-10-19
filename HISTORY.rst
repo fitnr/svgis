cfbacfec44eeaa61cc871df7c13a9c2b50184367
fitnr <fitnr@fakeisthenewreal>
2015-10-19 18:37:52 -0400

    v0.1.3

--- HISTORY.rst
+++ HISTORY.rst
@@ -1,3 +1,9 @@
+0.1.3
+-----
+
+* Add ability to read OSM files (if slowly)
+* bug fixes in reading, writing
+
 0.1.2
 -----
 
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ except IOError:
 setup(
     name='svgis',
 
-    version='0.1.2',
+    version='0.1.3',
 
     description='Draw geodata in SVG',
 
