f98f5db53095ed1f195e3b5968a0988a0f08255c
fitnr <fitnr@fakeisthenewreal>
2016-01-26 00:51:59 -0500

    fix up move

--- tests/profile.py
+++ tests/profile.py
@@ -8,7 +8,7 @@
 # http://www.opensource.org/licenses/GNU General Public License v3 (GPLv3)-license
 # Copyright (c) 2016, Neil Freeman <contact@fakeisthenewreal.org>
 
-from svgis.cli import actions
+from svgis import svgis
 
 if __name__ == '__main__':
     shp = 'tests/test_data/cb_2014_us_nation_20m.shp'
@@ -16,4 +16,4 @@ if __name__ == '__main__':
     PROJECTION = '+proj=lcc +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs'
     BOUNDS = (-124.0, 20.5, -64.0, 49.0)
 
-    actions.draw(shp, style=css, scale=1000, project=PROJECTION, bounds=BOUNDS, clip=None)
+    svgis.map(shp, style=css, scale=1000, project=PROJECTION, bounds=BOUNDS, clip=None)
