f7a812f49c02916b8b061dfd49f608608357c065
fitnr <fitnr@fakeisthenewreal>
2016-01-29 23:52:24 -0500

    Break css and dom manipulation apart
    roughly

---
+++ svgis/dom.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of svgis.
+# https://github.com/fitnr/svgis
+
+# Licensed under the GNU General Public License v3 (GPLv3) license:
+# http://opensource.org/licenses/GPL-3.0
+# Copyright (c) 2016, Neil Freeman <contact@fakeisthenewreal.org>
+
+'''
+Utilities for manipulating the DOM and applying styles to same.
+'''
+
+SVG_NS = 'http://www.w3.org/2000/svg'
+
+
+def cdata(text):
+    return '<![CDATA[' + text + ']]>'
+
+
+def ns(tag):
+    return '{' + SVG_NS + '}' + tag
+
+
+def apply_rule(doc, rule):
+    '''
+    Apply a tinycss Rule to an SVG document built by SVGIS.
+    '''
+    tokenlist = _build_tokenlist(rule.selector)
+
+    declaration = u' '.join(u'{}:{};'.format(d.name, d.value.as_css()) for d in rule.declarations)
+
+    for tokens in tokenlist:
+        # Starts as None as a marker to look into document,
+        # instead of filtering the list.
+        els = None
+        while len(tokens) > 0:
+            els, tokens = _process_tokens(doc, els, tokens)
+
+        for el in els:
+            el.attrib['style'] = el.attrib.get('style', u'') + declaration
+
+
+def _match_classes(elem_classes, rule_classes):
+    '''Check if rule_classes all fall in elem_classes.'''
+    return all([c in elem_classes for c in rule_classes])
+
+
+def _idrule(idname):
+    # Get rid of the hash here.
+    return "*[@id='{}']".format(idname[1:])
+
+
+def _build_tokenlist(tokens):
+    tokenlist = [[]]
+    itertokens = iter(tokens)
+
+    for token in itertokens:
+        if token.value == ',':
+            tokenlist.append([])
+            continue
+
+        if token.type == '[':
+            tokenlist.append([])
+            continue
+
+        # ignore a ':' and whatever comes after the ':'.
+        if token.value == ':':
+            if next(itertokens).value == ':':
+                next(itertokens)
+            continue
+
+        tokenlist[-1].append(token)
+
+    return tokenlist
+
+
+def _find_classes(doc, els, classes):
+    '''Get elements in doc that match given classes.'''
+    if els is None:
+        els = doc.iter()
+
+    return [e for e in els if _match_classes(e.attrib.get('class', []), classes)]
+
+
+def _get_elems_by_tagname(doc, els, tagname):
+    '''Find elems in els (or doc) that have the given tagname.'''
+    if els is None:
+        return doc.findall('.//' + tagname)
+    else:
+        return [e for e in els if e.tag == tagname]
+
+
+def _unsupported_token(token):
+    return token.type == 'DELIM' and token.value in ('+', '~', '')
+
+
+def _process_tokens(doc, els, tokens):
+    '''
+    Work through the given selector tokens, attempting to return the subset
+    of ``els`` that match.
+
+    Args:
+        doc (ElementTree.Element): Parent element to search within.
+        els (list): Current list of elements. For the first pass, this is None.
+        tokens (list): List of ``tinycss.Token``s.
+    '''
+    if any(_unsupported_token(t) for t in tokens):
+        return [], []
+
+    if tokens[0].value == '*':
+        els = doc.findall('.//')
+        remaining_tokens = tokens[1:]
+
+    # Look inside the given elements!
+    elif tokens[0].type == 'S':
+        try:
+            els = [u for e in els for u in e.findall('./')]
+            remaining_tokens = tokens[1:]
+
+        except TypeError:
+            return None, tokens[1:]
+
+    # Is there a <HASH>: it's an ID, find that, then look for other classes or tags.
+    elif any(t.type == 'HASH' for t in tokens):
+        idtokens = [t for t in tokens if t.type == 'HASH']
+
+        # Remove all the id hashes.
+        remaining_tokens = [t for t in tokens if t not in idtokens]
+
+        # Allow for multiple id hashes, but we only want the first one
+        els = doc.findall('.//' + _idrule(idtokens[0].value))
+
+    # first token is <DELIM '.'>: make a list of classes
+    elif tokens[0].value == '.' and tokens[0].type == 'DELIM':
+        classes = []
+
+        for tok in tokens:
+            if tok.type == 'S':
+                break
+
+            if tok.type == 'IDENT':
+                classes.append(tok.value)
+
+        els = _find_classes(doc, els, classes)
+        remaining_tokens = []
+
+    # first token is <IDENT>: it's an element, possibly followed by classes.
+    elif tokens[0].type == 'IDENT':
+        els = _get_elems_by_tagname(doc, els, ns(tokens[0].value))
+        remaining_tokens = tokens[1:]
+
+    return els, remaining_tokens
--- svgis/style.py
+++ svgis/style.py
@@ -9,33 +9,42 @@
 # Copyright (c) 2016, Neil Freeman <contact@fakeisthenewreal.org>
 
 import re
-
+from string import ascii_letters
+import os.path
+import logging
 try:
     import xml.etree.cElementTree as ElementTree
 except ImportError:
     import xml.etree.ElementTree as ElementTree
-import os.path
-import logging
 import tinycss
-
+from . import dom
 
 '''
 Utilities for messing with SVG styling.
 '''
 
-SVG_NS = 'http://www.w3.org/2000/svg'
-
-
 def _register():
-    ElementTree.register_namespace('', SVG_NS)
+    ElementTree.register_namespace('', dom.SVG_NS)
 
 
-def _cdata(text):
-    return '<![CDATA[' + text + ']]>'
+def sanitize(string):
+    '''
+    Make input safe of use in an svg ID or class field.
+    Replaces blocks of whitespace with an underscore (``_``).
+    If the first character isn't an ascii letter, dash (``-``)
+    or underscore (``_``), an underscore is added to the beginning.
 
+    Args:
+        string (mixed): Input to sanitize. Will be immediately coerced to
+                        unicode. If that fails, returns an empty string.
+    '''
+    try:
+        string = re.sub(r'\s+', u'_', unicode(string))
+        string = string if string[0] in ('_-' + ascii_letters) else '_' + string
+        return unicode(string)
 
-def _ns(tag):
-    return '{' + SVG_NS + '}' + tag
+    except (AttributeError, IndexError):
+        return u''
 
 
 def pick(style):
@@ -56,7 +65,7 @@ def pick(style):
         return None
 
     except IOError:
-        logging.getLogger('svg').warn("Couldn't read %s, proceeding with default style", style)
+        logging.getLogger('svgis').warn("Couldn't read %s, proceeding with default style", style)
 
     return style
 
@@ -66,7 +75,7 @@ def rescale(svgfile, factor):
     try:
         svg = ElementTree.parse(svgfile)
         scalar = 'scale({})'.format(factor)
-        g = svg.getroot().find(_ns('g'))
+        g = svg.getroot().find(dom.ns('g'))
         g.attrib['transform'] = (g.attrib.get('transform') + ' ' + scalar).strip()
 
     except AttributeError:
@@ -76,7 +85,8 @@ def rescale(svgfile, factor):
 
 
 def add_style(svgfile, style, replace=False):
-    '''Add to the CSS style in an SVG file.
+    '''
+    Add to or replace the CSS style in an SVG file.
 
     Args:
         svgfile (string): Path to an SVG file or an SVG string.
@@ -102,34 +112,34 @@ def add_style(svgfile, style, replace=False):
         except UnicodeDecodeError:
             svg = ElementTree.fromstring(svgfile.encode('utf-8'))
 
-    if svg.find(_ns('defs')) is not None:
-        defs = svg.find(_ns('defs'))
+    if svg.find(dom.ns('defs')) is not None:
+        defs = svg.find(dom.ns('defs'))
 
     else:
-        defs = ElementTree.Element(_ns('defs'))
+        defs = ElementTree.Element(dom.ns('defs'))
         svg.insert(0, defs)
 
-    if defs.find(_ns('style')) is not None:
-        style_element = defs.find(_ns('style'))
+    if defs.find(dom.ns('style')) is not None:
+        style_element = defs.find(dom.ns('style'))
 
         if not replace:
             # Append CSS.
             existing = style_element.text or ''
-            style = _cdata(existing + ' ' + style)
+            style = dom.cdata(existing + ' ' + style)
             style_element.text = ''
 
     else:
-        style_element = ElementTree.Element(_ns('style'))
+        style_element = ElementTree.Element(dom.ns('style'))
         defs.append(style_element)
 
-    style_element.text = _cdata(style)
+    style_element.text = dom.cdata(style)
 
     return ElementTree.tostring(svg, encoding='utf-8').decode('utf-8')
 
 
 def inline(svg, style=None):
     '''
-    Inline the CSS rules into the SVG. This is a very rough operation,
+    Inline the CSS rules in an SVG. This is a very rough operation,
     and full css precedence rules won't be respected.
 
     Args:
@@ -139,143 +149,25 @@ def inline(svg, style=None):
     _register()
     try:
         doc = ElementTree.fromstring(svg.encode('utf-8'))
-        wrap = doc.find('./' + _ns('g'))
+        wrap = doc.find('./' + dom.ns('g'))
 
         if not style:
-            path = './' + _ns('defs') + '/' + _ns('style')
+            path = './' + dom.ns('defs') + '/' + dom.ns('style')
             style = doc.findall(path).pop().text
 
-        stylesheet = tinycss.make_parser().parse_stylesheet(_minify(style))
+        stylesheet = _parse_css(style)
 
         for rule in stylesheet.rules:
-            tokenlist = _build_tokenlist(rule.selector)
-
-            declaration = u' '.join(u'{}:{};'.format(d.name, d.value.as_css()) for d in rule.declarations)
-
-            for tokens in tokenlist:
-                # Starts as None as a marker to look into document,
-                # instead of filtering the list.
-                els = None
-                while len(tokens) > 0:
-                    els, tokens = _process_tokens(wrap, els, tokens)
-
-                for el in els:
-                    el.attrib['style'] = el.attrib.get('style', u'') + declaration
+            dom.apply_rule(wrap, rule)
 
         return ElementTree.tostring(doc, encoding='utf-8').decode('utf-8')
 
     # Return plain old SVG.
-    except (AttributeError, NameError):
-        raise
+    except (AttributeError, NameError) as e:
+        logging.getLogger('svgis').warn("Unable to inline CSS becuase: %s", e)
+        return svg
 
+def _parse_css(stylesheet):
+    mini = re.sub(r',\s+', ',', stylesheet)
+    return tinycss.make_parser().parse_stylesheet(mini)
 
-def _minify(stylesheet):
-    return re.sub(r',\s+', ',', stylesheet)
-
-
-def _match_classes(elem_classes, rule_classes):
-    '''Check if rule_classes all fall in elem_classes.'''
-    return all([c in elem_classes for c in rule_classes])
-
-
-def _isgroup(el):
-    return el.tag == '{http://www.w3.org/2000/svg}g'
-
-
-def _idrule(idname):
-    # Get rid of the hash here.
-    return "*[@id='{}']".format(idname[1:])
-
-
-def _build_tokenlist(tokens):
-    tokenlist = [[]]
-    itertokens = iter(tokens)
-
-    for token in itertokens:
-        if token.value == ',':
-            tokenlist.append([])
-            continue
-
-        if token.type == '[':
-            tokenlist.append([])
-            continue
-
-        # ignore a ':' and whatever comes after the ':'.
-        if token.value == ':':
-            n = next(itertokens)
-            if n.value == ':':
-                next(itertokens)
-            continue
-
-        tokenlist[-1].append(token)
-
-    return tokenlist
-
-
-def _find_classes(doc, els, classes):
-    '''Get elements in doc that match given classes.'''
-    if els is None:
-        els = doc.iter()
-
-    return [e for e in els if _match_classes(e.attrib.get('class', []), classes)]
-
-
-def _get_elems_by_tagname(doc, els, tagname):
-    '''Find elems in els (or doc) that have the given tagname.'''
-    if els is None:
-        return doc.findall('.//' + tagname)
-    else:
-        return [e for e in els if e.tag == tagname]
-
-
-def _unsupported_token(token):
-    return token.type == 'DELIM' and token.value in ('+', '~', '')
-
-
-def _process_tokens(doc, els, tokens):
-    if any(_unsupported_token(t) for t in tokens):
-        return [], []
-
-    if tokens[0].value == '*':
-        els = doc.findall('.//')
-        remaining_tokens = tokens[1:]
-
-    # Look inside the given elements!
-    elif tokens[0].type == 'S':
-        try:
-            els = [u for e in els for u in e.findall('./')]
-            remaining_tokens = tokens[1:]
-
-        except TypeError:
-            return None, tokens[1:]
-
-    # Is there a <HASH>: it's an ID, find that, then look for other classes or tags.
-    elif any(t.type == 'HASH' for t in tokens):
-        idtokens = [t for t in tokens if t.type == 'HASH']
-
-        # Remove all the id hashes.
-        remaining_tokens = [t for t in tokens if t not in idtokens]
-
-        # Allow for multiple id hashes, but we only want the first one
-        els = doc.findall('.//' + _idrule(idtokens[0].value))
-
-    # first token is <DELIM '.'>: make a list of classes
-    elif tokens[0].value == '.' and tokens[0].type == 'DELIM':
-        classes = []
-
-        for tok in tokens:
-            if tok.type == 'S':
-                break
-
-            if tok.type == 'IDENT':
-                classes.append(tok.value)
-
-        els = _find_classes(doc, els, classes)
-        remaining_tokens = []
-
-    # first token is <IDENT>: it's an element, possibly followed by classes.
-    elif tokens[0].type == 'IDENT':
-        els = _get_elems_by_tagname(doc, els, _ns(tokens[0].value))
-        remaining_tokens = tokens[1:]
-
-    return els, remaining_tokens
