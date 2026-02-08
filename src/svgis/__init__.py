90cf21564a1d65f6368c25f3192d93b4a4aaade6
fitnr <fitnr@fakeisthenewreal>
2026-02-08 17:39:17 -0500

    v0.5.4

--- src/svgis/__init__.py
+++ src/svgis/__init__.py
@@ -8,7 +8,7 @@
 from . import bounding, draw, errors, projection, style, svg, svgis, transform
 from .svgis import SVGIS, map
 
-__version__ = '0.5.3'
+__version__ = '0.5.4'
 
 __all__ = [
     'bounding',
