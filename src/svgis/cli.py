0d56bb7b10ed592c0d187727242adb407ddbd819
fitnr <fitnr@fakeisthenewreal>
2026-09-13 15:06:04 -0400

    Typo

--- src/svgis/cli.py
+++ src/svgis/cli.py
@@ -187,7 +187,7 @@ def bounds(layer, crs, latlon=False):
     help='Rounding precision for coordinates (default: 5)',
 )
 @click.option('--clip/--no-clip', ' /-n', **clipkwargs)
-@click.option('--inline/--no-inline', '-l/ ' ** csskwargs)
+@click.option('--inline/--no-inline', '-l/ ', **csskwargs)
 @click.option('--viewbox/--no-viewbox', ' /-x', default=False, help='Draw SVG using a ViewBox (default: no ViewBox)')
 @click.option('-q', '--quiet', default=False, flag_value=True, help='Ignore warnings')
 @click.option('-v', '--verbose', default=False, count=True, help='Talk a lot')
